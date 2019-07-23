63024aee291c4c5127e24442360dbd0443d2845b
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-07-23 15:42:24 +0300

    Enhanced gitignore & rehaped moving mnist dataset

--- moving_mnist/loader.py
+++ moving_mnist/loader.py
@@ -1,7 +1,13 @@
 import numpy as np
 
-DATASET_PATH = 'C:\DOC\Workspace\mnist_test_seq.npy'
+# Dataset downloaded from http://www.cs.toronto.edu/~nitish/unsupervised_video/mnist_test_seq.npy
+# Documentation at http://www.cs.toronto.edu/~nitish/unsupervised_video/
+DATASET_PATH = '../../mnist_test_seq.npy'
 
+# Shape (20, 10000, 64, 64) sequence length, nr. of sequences, image sizes
 dataset = np.load(DATASET_PATH)
 
-print(dataset)
+# # Shape (10000, 20, 64, 64) nr. of sequences, seq length, image sizes
+dataset = np.swapaxes(dataset, 0, 1)
+
+print(dataset[0].shape)
