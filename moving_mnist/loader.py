f1c282063e1cea9fc6a385bb144514c84a084f6f
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-07-24 10:34:35 +0300

    Plot animation out of frames

--- moving_mnist/loader.py
+++ moving_mnist/loader.py
@@ -1,11 +1,8 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+import util
 
 # Dataset downloaded from http://www.cs.toronto.edu/~nitish/unsupervised_video/mnist_test_seq.npy
 # Documentation at http://www.cs.toronto.edu/~nitish/unsupervised_video/
-from torch.utils import data
-
 DATASET_PATH = '../../mnist_test_seq.npy'
 
 # Shape (20, 10000, 64, 64) sequence length, nr. of sequences, image sizes
@@ -14,5 +11,9 @@ dataset = np.load(DATASET_PATH)
 # Shape (10000, 20, 64, 64) nr. of sequences, seq length, image sizes
 dataset = np.swapaxes(dataset, 0, 1)
 
-imgplot = plt.imshow(dataset[0][0], cmap='gray', vmin=0, vmax=255)
-plt.show()
+# Plot a single image
+# util.plot_grayscale(dataset[1][1])
+
+# Plot animation
+util.plot_animation(dataset[1])
+
---
+++ moving_mnist/util.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+# Plot a single image from numpy array of shape n x m
+def plot_grayscale(img):
+    fig = plt.figure()
+    imgplot = plt.imshow(img, cmap='gray', vmin=0, vmax=255)
+    plt.show()
+
+# Plot an animation out of numpy array of shape frames x n x m
+def plot_animation(frames):
+    fig = plt.figure()
+    ims = []
+    for i in range(len(frames)):
+        im = plt.imshow(frames[i], cmap='gray', vmin=0, vmax=255)
+        ims.append([im])
+    ani = animation.ArtistAnimation(fig, ims, interval=100, blit=True, repeat_delay=1000)
+    plt.show()
