07a673426da75b463abcb17df7de519df5a1dfdf
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-07-26 15:11:03 +0300

    Refactor loader

--- moving_mnist/loader.py
+++ moving_mnist/loader.py
@@ -5,15 +5,16 @@ import util
 # Documentation at http://www.cs.toronto.edu/~nitish/unsupervised_video/
 DATASET_PATH = '../../mnist_test_seq.npy'
 
-# Shape (20, 10000, 64, 64) sequence length, nr. of sequences, image sizes
-dataset = np.load(DATASET_PATH)
+def load_dataset():
+    # Shape (20, 10000, 64, 64) sequence length, nr. of sequences, image sizes
+    dataset = np.load(DATASET_PATH)
+    # Shape (10000, 20, 64, 64) nr. of sequences, seq length, image sizes
+    dataset = np.swapaxes(dataset, 0, 1)
+    return dataset
 
-# Shape (10000, 20, 64, 64) nr. of sequences, seq length, image sizes
-dataset = np.swapaxes(dataset, 0, 1)
-
-# Plot a single image
-# util.plot_grayscale(dataset[1][1])
-
-# Plot animation
-util.plot_animation(dataset[1])
-
+if __name__ == '__main__':
+    dataset = load_dataset()
+    # Plot a single image
+    # util.plot_grayscale(dataset[1, 1])
+    # Plot animation
+    util.plot_animation(dataset[2])
