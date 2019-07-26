b9b066ca8474409f2211bd0aa9abd90d01c3e60a
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-07-26 15:55:32 +0300

    Plot 3 slices frames

--- moving_mnist/loader.py
+++ moving_mnist/loader.py
@@ -12,9 +12,33 @@ def load_dataset():
     dataset = np.swapaxes(dataset, 0, 1)
     return dataset
 
+def load_3_frames_slices():
+    dataset = load_dataset()
+    for frames in dataset:
+        for i in range(len(frames) - 2):
+            yield ((frames[i], frames[i + 2]), frames[i + 1])
+
+def plot_animation_3_slices(limit = 2):
+    frames = []
+    for x, y in load_3_frames_slices():
+        if limit == 0:
+            break
+        frame_1, frame_3 = x
+        frame_2 = y
+        frames.append(frame_1)
+        frames.append(frame_2)
+        frames.append(frame_3)
+        limit -= 1
+    util.plot_animation(frames)
+
 if __name__ == '__main__':
     dataset = load_dataset()
+
     # Plot a single image
     # util.plot_grayscale(dataset[1, 1])
+
     # Plot animation
-    util.plot_animation(dataset[2])
+    # util.plot_animation(dataset[2])
+
+    # Plot animation of 3 slices
+    plot_animation_3_slices()
