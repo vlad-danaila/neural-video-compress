c5667f56692d713eb4fd469c071d38c4790ca32d
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-07-30 16:46:38 +0300

    Added learning rate scheduler

--- moving_mnist/simple_cnn.py
+++ moving_mnist/simple_cnn.py
@@ -10,8 +10,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 device = t.device("cuda:0" if t.cuda.is_available() else "cpu")
-# PATH = '/content/drive/My Drive/mnist_test_seq.npy'
-PATH = loader.DATASET_PATH
+PATH = '/content/drive/My Drive/mnist_test_seq.npy'
+# PATH = loader.DATASET_PATH
 
 class MinimalCNN(t.nn.Module):
     def __init__(self):
@@ -26,29 +26,30 @@ def train_minimal_cnn(path = loader.DATASET_PATH):
     minimalCNN.to(device)
     dataloader = DataLoader(loader.MovingMNIST3Frames(path), batch_size=30, shuffle=False)
     criterion = t.nn.L1Loss()
-    optimizer = optim.SGD(minimalCNN.parameters(), lr=0.0001, momentum=0.9)
+    optimizer = optim.SGD(minimalCNN.parameters(), lr=0.1, momentum=0.9)
+    scheduler = t.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
     running_loss = 0.0
+    for j in range(5):
+      for i, data in enumerate(dataloader):
+          # x shape = 50 x 2 x 64 x 64
+          # y shape = 50 x 1 x 64 x 64
+          x, y_real = data[0].to(device), data[1].to(device)
 
-    for i, data in enumerate(dataloader):
-        # x shape = 50 x 2 x 64 x 64
-        # y shape = 50 x 1 x 64 x 64
-        x, y_real = data[0].to(device), data[1].to(device)
+          # zero the parameter gradients
+          optimizer.zero_grad()
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+          # forward + backward + optimize
+          y_pred = minimalCNN(x)
+          loss = criterion(y_pred, y_real)
+          loss.backward()
+          optimizer.step()
 
-        # forward + backward + optimize
-        y_pred = minimalCNN(x)
-        loss = criterion(y_pred, y_real)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 100 == 0:  # print every 100 mini-batches
-            print('[%d] loss: %.3f' % (i, running_loss / 10))
-            running_loss = 0.0
-
+          # print statistics
+          running_loss += loss.item()
+          if i % 100 == 0:  # print every 100 mini-batches
+              print('[%d] loss: %.3f' % (i, running_loss / 10))
+              running_loss = 0.0
+      scheduler.step()
     return minimalCNN
 
 def plot_3_frames(model):
@@ -61,6 +62,6 @@ def plot_3_frames(model):
     [util.plot_grayscale(f) for f in [frame_1, frame_2, frame_3]]
 
 if __name__ == '__main__':
-    # model = train_minimal_cnn(PATH)
-    model = MinimalCNN()
+    model = train_minimal_cnn(PATH)
+#     model = MinimalCNN()
     plot_3_frames(model)
