66c9937799820623004428e8f174c4f5d2de17b6
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-07-30 17:13:39 +0300

    Added simple cnn with fully connected layers.

--- moving_mnist/simple_cnn.py
+++ moving_mnist/simple_cnn.py
@@ -10,8 +10,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 device = t.device("cuda:0" if t.cuda.is_available() else "cpu")
-PATH = '/content/drive/My Drive/mnist_test_seq.npy'
-# PATH = loader.DATASET_PATH
+# PATH = '/content/drive/My Drive/mnist_test_seq.npy'
+PATH = loader.DATASET_PATH
 
 class MinimalCNN(t.nn.Module):
     def __init__(self):
@@ -20,6 +20,27 @@ class MinimalCNN(t.nn.Module):
     def forward(self, x):
         x = self.conv1(x)
         return x
+
+class SimpleCNN(t.nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = t.nn.Conv2d(in_channels=2, out_channels=6, kernel_size=5, padding=2)
+        # self.conv1 = t.nn.Conv2d(3, 6, 5)
+        self.pool = t.nn.MaxPool2d(2, 2)
+        self.conv2 = t.nn.Conv2d(in_channels=6, out_channels=11, kernel_size=5, padding=2)
+        # self.conv2 = t.nn.Conv2d(6, 16, 5)
+        self.fc1 = t.nn.Linear(11 * 16 * 16, 64 * 64 * 5)
+        self.fc2 = t.nn.Linear(64 * 64 * 5, 64 * 64 * 3)
+        self.fc3 = t.nn.Linear(64 * 64 * 3, 64 * 64)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 11 * 16 * 16)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x.view(-1, 1, 64, 64)
 
 def train_minimal_cnn(path = loader.DATASET_PATH):
     minimalCNN = MinimalCNN()
@@ -62,6 +83,6 @@ def plot_3_frames(model):
     [util.plot_grayscale(f) for f in [frame_1, frame_2, frame_3]]
 
 if __name__ == '__main__':
-    model = train_minimal_cnn(PATH)
-#     model = MinimalCNN()
+    # model = train_minimal_cnn(PATH)
+    model = SimpleCNN()
     plot_3_frames(model)
