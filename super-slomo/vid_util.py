3f7fd35c88c87244eda50d3ace3784eede2f0601
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2020-03-30 15:27:09 +0300

    vid uitils

--- super-slomo/vid_util.py
+++ super-slomo/vid_util.py
@@ -9,6 +9,14 @@ SOMETHING_SOMETHING_DIR = 'Something_something'
 PATH_SOMETHING_SOMETHING_RAW = join(RAW_VIDEO_PATH, SOMETHING_SOMETHING_DIR)
 PATH_SOMETHING_SOMETHING_FRAMES = join(FRAMES_PATH, SOMETHING_SOMETHING_DIR)
 
+CHARDES_DIR = 'Chardes'
+PATH_CHARDES_RAW = join(RAW_VIDEO_PATH, CHARDES_DIR)
+PATH_CHARDES_FRAMES = join(FRAMES_PATH, CHARDES_DIR)
+
+EGO_DIR = 'ChardesEgo'
+PATH_EGO_RAW = join(RAW_VIDEO_PATH, EGO_DIR)
+PATH_EGO_FRAMES = join(FRAMES_PATH, EGO_DIR)
+
 def check_error(status_code, error_msg):
     if status_code > 0:
         raise Exception(error_msg)
@@ -20,7 +28,6 @@ def make_dir_if_missing(dir):
 def make_frames_dir():
     assert exists(PATH_SOMETHING_SOMETHING_RAW)
     make_dir_if_missing(FRAMES_PATH)
-    make_dir_if_missing(PATH_SOMETHING_SOMETHING_FRAMES)
 
 def extract_frames(in_file, out_file, fps):
     cmd = 'ffmpeg -i "{in_file}" -r {fps}/1 "{out_file}{separator}%03d.jpg"'\
@@ -42,5 +49,6 @@ def frames_from_videos(videos_dir, frames_dir, counter = 0):
 
 if __name__ == '__main__':
     make_frames_dir()
-    counter = frames_from_videos(PATH_SOMETHING_SOMETHING_RAW, PATH_SOMETHING_SOMETHING_FRAMES)
-
+    counter = frames_from_videos(PATH_SOMETHING_SOMETHING_RAW, FRAMES_PATH, counter = 148093)
+    counter = frames_from_videos(PATH_CHARDES_RAW, FRAMES_PATH, counter=counter)
+    counter = frames_from_videos(PATH_EGO_RAW, FRAMES_PATH, counter=counter)
